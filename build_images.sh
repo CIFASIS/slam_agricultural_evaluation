04ec5c4b0c18342b269fe3663b204974cf87b4f9
Román Comelli <roman.comelli@gmail.com>
2020-11-25 23:41:58 -0300

    Change source for execute of image building scripts

--- build_images.sh
+++ build_images.sh
@@ -8,5 +8,5 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for m in ${MODULES[@]};
 do
   echo "Building $m ..."
-  . $CURRENT_DIR/$m/run.sh -b
+  $CURRENT_DIR/$m/run.sh -b
 done
