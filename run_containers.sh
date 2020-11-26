64dd0b792ba4b665079755113b6d7f87d3664ca4
Román Comelli <roman.comelli@gmail.com>
2020-11-26 10:21:13 -0300

    Change source for execute of run_all_sequences.sh scripts and add INT trap and minimal argument checking in run_containers.sh

--- run_containers.sh
+++ run_containers.sh
@@ -5,12 +5,19 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ "$#" -ne 1 ]; then
+  echo "Path of Rosario Dataset should be provided as (the only) argument" >&2
+  exit 1
+fi
+
 DATASET_DIR=$1
 
 . $CURRENT_DIR/modules.config
 
+trap "exit 1" INT
+
 for m in ${MODULES[@]};
 do
   echo "Running $m ..."
-  . $CURRENT_DIR/$m/run_all_rosario_sequences.sh $DATASET_DIR
+  $CURRENT_DIR/$m/run_all_rosario_sequences.sh $DATASET_DIR
 done
